c3ef3da12cea6a87195761b203822cabc1dd4bb5
lexun1060 <getelementbyidkango@gmail.com>
2020-09-24 10:37:03 +0300

    completed schema model and test

--- server/test/api.test.js
+++ server/test/api.test.js
@@ -3,23 +3,67 @@ const request = require('supertest');
 const app = require('../src/app');
 
 describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: 'API - 👋🌎🌍🌏'
-      }, done);
-  });
+    it('responds with a json message', (done) => {
+        request(app)
+            .get('/api/v1')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(200, {
+                message: 'API - 👋🌎🌍🌏'
+            }, done);
+    });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
-    request(app)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
-  });
-});
+describe('POST /api/v1/messages', () => {
+    it('responds with inserted message', (done) => {
+        const requestObj = {
+            name: 'CJ',
+            message: 'This',
+            latitude: -90,
+            longitude: 180
+        }
+
+        const responseObj = {
+            ...requestObj,
+            _id: '5b57d127923211248855977c',
+            date: '2018-07-25T01:23:51.029Z'
+        }
+
+        request(app)
+            .post('/api/v1/messages')
+            .send(requestObj)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(res => {
+                console.log(res.body, 'body')
+                res.body._id = '5b57d127923211248855977c';
+                res.body.date = '2018-07-25T01:23:51.029Z';
+            })
+            .expect(200, responseObj, done());
+    });
+
+    it('can signup with a name that has diacrictics', (done) => {
+        const requestObj = {
+            name: 'Yahoo',
+            message: 'This app is so cool',
+            latitude: -90,
+            longitude: 180
+        }
+        const responseObj = {
+            ...requestObj,
+            _id: '5b57d127923211248855977c',
+            date: '2018-07-25T01:23:51.029Z'
+        }
+        request(app)
+            .post('/api/v1/messages')
+            .send(requestObj)
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(res => {
+                console.log(res.body, 'body two it')
+                res.body._id = '5b57d127923211248855977c';
+                res.body.date = '2018-07-25T01:23:51.029Z';
+            })
+            .expect(200, responseObj, done());
+    });
+})
